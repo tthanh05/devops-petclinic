d5aeab1bca45969a893021d68cbc1e81ba2b0926
tthanh05 <lutuanthanh2017@gmail.com>
2025-10-01 11:16:08 +1000

    Update unit test
    Signed-off-by: tthanh05 <lutuanthanh2017@gmail.com>

--- src/test/java/org/springframework/samples/petclinic/owner/PetValidatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetValidatorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PetValidatorTest {
@@ -12,17 +14,28 @@ class PetValidatorTest {
   void rejectsEmptyName() {
     Pet pet = new Pet();
     pet.setName(""); // invalid
+    pet.setBirthDate(LocalDate.now());
+    PetType type = new PetType();
+    type.setName("dog");
+    pet.setType(type);
+
     Errors errors = new BeanPropertyBindingResult(pet, "pet");
 
     new PetValidator().validate(pet, errors);
 
+    // Still specifically asserts name is invalid (even if others are valid)
     assertThat(errors.hasFieldErrors("name")).isTrue();
   }
 
   @Test
   void acceptsValidPet() {
     Pet pet = new Pet();
-    pet.setName("Leo");
+    pet.setName("Leo"); // valid
+    pet.setBirthDate(LocalDate.of(2020, 1, 15)); // valid
+    PetType type = new PetType();
+    type.setName("cat"); // valid
+    pet.setType(type);
+
     Errors errors = new BeanPropertyBindingResult(pet, "pet");
 
     new PetValidator().validate(pet, errors);
